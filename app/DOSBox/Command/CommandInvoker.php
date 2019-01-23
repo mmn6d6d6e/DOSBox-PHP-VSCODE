d1fb34c053c958e46653e75d20a9f2d745a46641
nuansa <nuansa@stikom.edu>
2019-01-23 10:47:02 +0700

    exit tanpa error

--- app/DOSBox/Command/CommandInvoker.php
+++ app/DOSBox/Command/CommandInvoker.php
@@ -40,6 +40,7 @@ class CommandInvoker implements IExecuteCommand {
                     return;
                 }
             }
+            if($command!='exit')
             $outputter->printLine("'{$command}' is not recognized as an internal or external command, operable program or batch file.");
         } catch(Exception $e){
             if($e->getMessage() != null) {
