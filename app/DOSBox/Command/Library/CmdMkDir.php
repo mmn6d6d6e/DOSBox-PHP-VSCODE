ff60c27c1a8fa536b46686d3e468a46561885fef
mmn13 <mmn.die.at.13@gmail.com>
2019-01-23 15:40:39 +0700

    Fix: bug MKDIR & MKFILE bisa create file/dir dengan nama yang sudah ada
    Menambahkan pengecekan di ::execute()

--- app/DOSBox/Command/Library/CmdMkDir.php
+++ app/DOSBox/Command/Library/CmdMkDir.php
@@ -9,6 +9,9 @@ use DOSBox\Command\BaseCommand as Command;
 
 class CmdMkDir extends Command {
     const PARAMETER_CONTAINS_BACKLASH = "At least one parameter denotes a path rather than a directory name.";
+    const MSG_FAIL = 'Cannot create folder!';
+    const MSG_FILE_EXISTS = self::MSG_FAIL.' File with same name already exists!';
+    const MSG_FOLDER_EXISTS = self::MSG_FAIL.' Folder with same name already exists!';
 
     public function __construct($commandName, IDrive $drive){
         parent::__construct($commandName, $drive);
@@ -39,6 +42,20 @@ class CmdMkDir extends Command {
 
     public function execute(IOutputter $outputter){
         for($i=0; $i < $this->getParameterCount(); $i++) {
+            $fileName = $this->params[$i];
+            $listItems = $this->getDrive()->getCurrentDirectory()->getContent();
+            foreach($listItems as $item){
+                if($item->getName() == $fileName){
+                    if($item->isDirectory()){
+                        $outputter->printNoLine(self::MSG_FOLDER_EXISTS);
+                        $outputter->newLine();
+                        return;
+                    }
+                    $outputter->printNoLine(self::MSG_FILE_EXISTS);
+                    $outputter->newLine();
+                    return;
+                }
+            }
             $this->createDirectory($this->params[$i], $this->getDrive());
         }
     }
--- app/DOSBox/Command/Library/CmdMkFile.php
+++ app/DOSBox/Command/Library/CmdMkFile.php
@@ -8,6 +8,10 @@ use DOSBox\Filesystem\File;
 use DOSBox\Command\BaseCommand as Command;
 
 class CmdMkFile extends Command {
+    const MSG_FAIL = 'Cannot create file!';
+    const MSG_FILE_EXISTS = self::MSG_FAIL.' File with same name already exists!';
+    const MSG_FOLDER_EXISTS = self::MSG_FAIL.' Folder with same name already exists!';
+
     public function __construct($commandName, IDrive $drive){
         parent::__construct($commandName, $drive);
     }
@@ -22,6 +26,19 @@ class CmdMkFile extends Command {
 
     public function execute(IOutputter $outputter){
         $fileName = $this->params[0];
+        $listItems = $this->getDrive()->getCurrentDirectory()->getContent();
+        foreach($listItems as $item){
+            if($item->getName() == $fileName){
+                if($item->isDirectory()){
+                    $outputter->printNoLine(self::MSG_FOLDER_EXISTS);
+                    $outputter->newLine();
+                    return;
+                }
+                $outputter->printNoLine(self::MSG_FILE_EXISTS);
+                $outputter->newLine();
+                return;
+            }
+        }
         $fileContent = isset($this->params[1]) ? $this->params[1] : '';
         $newFile = new File($fileName, $fileContent);
         $this->getDrive()->getCurrentDirectory()->add($newFile);
