477862066a2fa356b992f3583f976c17dfca4a67
mmn13 <mmn.die.at.13@gmail.com>
2019-01-24 11:18:27 +0700

    Isi command CmdCopy
    - Tambah pengecekan param
    - Isi execute
    
    note: error handling minimal

--- app/DOSBox/Command/Library/CmdCopy.php
+++ app/DOSBox/Command/Library/CmdCopy.php
@@ -4,25 +4,45 @@ namespace DOSBox\Command\Library;
 
 use DOSBox\Interfaces\IDrive;
 use DOSBox\Interfaces\IOutputter;
+use DOSBox\Filesystem\File;
 
 use DOSBox\Command\BaseCommand as Command;
 
 class CmdCopy extends Command {
-    public $executed = false;
+    const MSG_FAILED = 'Cannot copy file!';
+    const MSG_FILE_EXISTS = self::MSG_FAILED.' File with same name already exists!';
+    const MSG_FILE_NOT_EXISTS = self::MSG_FAILED.' File not exists!';
+
+    private $sourceFile = null;
+    private $targetDir = null;
 
     public function __construct($commandName, IDrive $drive){
         parent::__construct($commandName, $drive);
     }
 
     public function checkNumberOfParameters($numberOfParametersEntered) {
-        return true;
+        return $numberOfParametersEntered == 2 ? true : false;
     }
 
     public function checkParameterValues(IOutputter $outputter) {
         return true;
     }
 
-    public function execute(IOutputter $outputter){
-        $this->executed = true;
+    public function execute(IOutputter $outputter) {
+        // copied file
+        $fileName = $this->getParameterAt(0);
+        $currentDir = $this->getDrive()->getCurrentDirectory();
+        $filteredFiles = array_filter($currentDir->getContent(), function($fileItem) use ($fileName) {
+            return $fileItem->getName() == $fileName;
+        });
+        $sourceFile = $filteredFiles[0];
+
+        // target directory
+        $dirName = $this->getParameterAt(1);
+        $targetDir = $this->getDrive()->getItemFromPath($dirName);
+
+        // write copied file to target directory
+        $newFile = new File($sourceFile->getName(), $sourceFile->getFileContent());
+        $targetDir->add($newFile);
     }
 }
