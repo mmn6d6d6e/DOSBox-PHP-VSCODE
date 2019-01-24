1334ef9bd848cf1b1ce6773d8dfddfa054d95382
mmn13 <mmn.die.at.13@gmail.com>
2019-01-24 09:59:21 +0700

    Isi CmdType
    - Tambah fungsional untuk cek param
    - Set data $targetFile
    - Print isi file

--- app/DOSBox/Command/Library/CmdType.php
+++ app/DOSBox/Command/Library/CmdType.php
@@ -8,21 +8,33 @@ use DOSBox\Interfaces\IOutputter;
 use DOSBox\Command\BaseCommand as Command;
 
 class CmdType extends Command {
-    public $executed = false;
+    private $targetFile;
 
     public function __construct($commandName, IDrive $drive){
         parent::__construct($commandName, $drive);
     }
 
     public function checkNumberOfParameters($numberOfParametersEntered) {
-        return true;
+        return $numberOfParametersEntered == 1 ? true : false;
     }
 
     public function checkParameterValues(IOutputter $outputter) {
+        $fileName = $this->getParameterAt(0);
+        $currentDir = $this->getDrive()->getCurrentDirectory();
+        $this->targetFile = array_filter($currentDir->getContent(), function ($file) use ($fileName) {
+            return $file->getName() == $fileName;
+        })[0];
+        // TODO: untuk pak Nuansa, pengecekan sebelum eksekusi di sini
         return true;
+
     }
 
     public function execute(IOutputter $outputter){
-        $this->executed = true;
+        $this->printContent($this->targetFile, $outputter);
+    }
+
+    protected function printContent($targetFile, IOutputter $outputter){
+        $outputter->printNoLine($targetFile->getName());
+        $outputter->newLine();
     }
 }
